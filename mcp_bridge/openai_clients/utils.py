a0cf387058af746bb95e4fd1306fb5ce20e86186
James Brown <randomvoidmail@foxmail.com>
2025-01-30 21:14:53 +0800

    Update utils.py

--- mcp_bridge/openai_clients/utils.py
+++ mcp_bridge/openai_clients/utils.py
@@ -9,6 +9,21 @@ from mcp_bridge.mcp_clients.McpClientManager import ClientManager
 from mcp_bridge.tool_mappers import mcp2openai
 
 
+def validate_if_json_object_parsable(content: str):
+    try:
+        json.loads(content)
+        return True
+    except ValueError:
+        return False
+
+
+def salvage_parsable_json_object(content: str):
+    content = content.strip()
+    for i in range(0, len(content)):
+        snippet = content[: len(content) - i]
+        if validate_if_json_object_parsable(snippet):
+            return snippet
+            
 async def chat_completion_add_tools(request: CreateChatCompletionRequest):
     request.tools = []
     logger.info("adding tools to request")
